a816c9e26766747a3d63e52fa848bab5d5f1cfec
driftshieldai <driftshieldai@gmail.com>
2025-12-19 19:21:33 +0000

    Update main.tf

--- terraform-jobs/main.tf
+++ terraform-jobs/main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "submit_vertex_job" {
   triggers = {
     dir_sha = sha256(join("", [
       for f in fileset("${path.module}/../train", "**") : 
-      filesha256("${path.module}/../train/${f}")
+      filesha256("${path.module}/../train/${}")
     ]))
   }
 
@@ -14,7 +14,7 @@ resource "null_resource" "submit_vertex_job" {
         --region=${var.region} \
         --display-name="driftshieldai-training-${formatdate("YYYYMMDD-hhmm", timestamp())}" \
         --worker-pool-spec=machine-type=n1-standard-4,replica-count=1,container-image-uri=us-central1-docker.pkg.dev/${var.project_id}/${var.repo_name}/${var.training_image_name}:latest \
-        --service-account:${var.service_account_id}}
+        --service-account=${var.service_account_id}
     EOT
+  }
 }
-
