bdc396466b91b2ed170d74ccbb2eb3e59a9cad51
driftshieldai <driftshieldai@gmail.com>
2025-12-19 18:35:04 +0000

    Create variables.tf

---
+++ terraform-jobs/variables.tf
@@ -0,0 +1,28 @@
+variable "project_id" {
+  description = "The GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "bucket_name" {
+  description = "Name of the GCS bucket"
+  type        = string
+}
+
+variable "repo_name" {
+  description = "Name of the Artifact Registry Repo"
+  type        = string
+  default     = "driftshield-ai-docker"
+}
+
+variable "image_name" {
+  description = "Name of the Artifact Registry training Repo"
+  type        = string
+  default     = "vertexai-custom"
+}
+
