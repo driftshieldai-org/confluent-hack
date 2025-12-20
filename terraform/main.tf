b5b3731d465496561f0b0be32b03edad226caee6
driftshieldai <driftshieldai@gmail.com>
2025-12-20 14:22:52 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,4 +102,5 @@ resource "google_compute_subnetwork" "subnet" {
   ip_cidr_range = "10.0.1.0/24"
   region        = var.region
   network       = google_compute_network.vpc_network.id
+  private_ip_google_access = true
 }
