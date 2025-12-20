8282d61907229bbc4db37020699cb85904170d31
driftshieldai <driftshieldai@gmail.com>
2025-12-20 10:06:59 +0000

    Update main.tf

--- terraform-jobs/main.tf
+++ terraform-jobs/main.tf
@@ -1,20 +1,26 @@
-resource "null_resource" "submit_vertex_job" {
-  # Trigger the job whenever the container image digest changes
-  triggers = {
-    dir_sha = sha256(join("", [
-      for f in fileset("${path.module}/../train", "**") : 
-      filesha256("${path.module}/../train/${f}")
-    ]))
-  }
+#resource "null_resource" "submit_vertex_job" {
+#  # Trigger the job whenever the container image digest changes
+#  triggers = {
+#    dir_sha = sha256(join("", [
+#      for f in fileset("${path.module}/../train", "**") : 
+#      filesha256("${path.module}/../train/${f}")
+#    ]))
+#  }
+#
+#  provisioner "local-exec" {
+#    command = <<EOT
+#      gcloud ai custom-jobs create \
+#        --project=${var.project_id} \
+#        --region=${var.region} \
+#        --display-name="driftshieldai-training-${formatdate("YYYYMMDD-hhmm", timestamp())}" \
+#        --worker-pool-spec=machine-type=n1-standard-1,replica-count=1,container-image-uri=us-central1-docker.pkg.dev/${var.project_id}/${var.repo_name}/${var.training_image_name}:latest \
+#        --service-account=${var.service_account_id}
+#    EOT
+#  }
+#}
 
-  provisioner "local-exec" {
-    command = <<EOT
-      gcloud ai custom-jobs create \
-        --project=${var.project_id} \
-        --region=${var.region} \
-        --display-name="driftshieldai-training-${formatdate("YYYYMMDD-hhmm", timestamp())}" \
-        --worker-pool-spec=machine-type=n1-standard-1,replica-count=1,container-image-uri=us-central1-docker.pkg.dev/${var.project_id}/${var.repo_name}/${var.training_image_name}:latest \
-        --service-account=${var.service_account_id}
-    EOT
-  }
+resource "google_storage_bucket_object" "template" {
+  bucket = var.bucket_id
+  name   = "dags/template/realtime_stream_anomaly.json"
+  source = "dataflow/template/realtime_stream_anomaly.json"
 }
