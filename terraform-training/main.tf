aa6d6b5529652f20c526ff60ae6be0021b54d965
driftshieldai <driftshieldai@gmail.com>
2025-12-21 19:12:01 +0000

    Update main.tf

--- terraform-training/main.tf
+++ terraform-training/main.tf
@@ -1,1 +1,20 @@
+resource "null_resource" "submit_vertex_job" {
+  # Trigger the job whenever the container image digest changes
+  triggers = {
+    dir_sha = sha256(join("", [
+      for f in fileset("${path.module}/../train", "**") : 
+      filesha256("${path.module}/../train/${f}")
+    ]))
+  }
 
+  provisioner "local-exec" {
+    command = <<EOT
+      gcloud ai custom-jobs create \
+        --project=${var.project_id} \
+        --region=${var.region} \
+        --display-name="driftshieldai-training-${formatdate("YYYYMMDD-hhmm", timestamp())}" \
+        --worker-pool-spec=machine-type=n1-standard-1,replica-count=1,container-image-uri=us-central1-docker.pkg.dev/${var.project_id}/${var.repo_name}/${var.training_image_name}:latest \
+        --service-account=${var.service_account_id}
+    EOT
+  }
+}
